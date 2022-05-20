e4f0b960c758a37cf7d7ef18653421dc6d304e0b
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-05-20 20:51:05 +0530

    #include<stdio.h> int main() {     int sum=0,size;     scanf("%d",&size);     int arr[size],i;     for(i=0;i<size;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<size;i++)     {         if(arr[size-i-1]%2!=0 && arr[size-i-3]%2!=0)         {             if(arr[size-i-2]%2!=0)             {                 sum=sum+1;             }         }     }     printf("%d",sum); }

---
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int sum=0,size;
+    scanf("%d",&size);
+    int arr[size],i;
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<size;i++)
+    {
+        if(arr[size-i-1]%2!=0 && arr[size-i-3]%2!=0)
+        {
+            if(arr[size-i-2]%2!=0)
+            {
+                sum=sum+1;
+            }
+        }
+    }
+    printf("%d",sum);
+}
