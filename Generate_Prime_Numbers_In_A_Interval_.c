fa6d9efbe6a96a5500f03f569b5ba57c48b516dc
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-05-25 21:27:33 +0530

    #include<stdio.h> int primes_in_range(int num) {     int i,count=0;     for(i=1;i<=num;i++)     {         if(num%i==0)         {             count++;         }     }     if(count==2)     {         return 1;     }     else     {         return 0;     } } int main() {     int a,b;     scanf("%d%d",&a,&b);     for(a;a<=b;a++)     {         if(primes_in_range(a)==1)       {        printf("%d ",a);       }     } }

---
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int primes_in_range(int num)
+{
+    int i,count=0;
+    for(i=1;i<=num;i++)
+    { 
+        if(num%i==0)
+        {
+            count++;
+        }
+    }
+    if(count==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    for(a;a<=b;a++)
+    {
+        if(primes_in_range(a)==1) 
+      {
+       printf("%d
+",a);
+      }
+    }
+}
