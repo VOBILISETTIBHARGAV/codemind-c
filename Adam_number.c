0b31d3378bd4148d0fc82b266412f5fd420c2505
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-05-28 19:18:50 +0530

    #include<stdio.h> int main() {     int n,sum=0,m,x,temp,semp,rev=0;     scanf("%d",&n);     m=n*n;     while(n>0)     {         temp=n%10;         sum=(sum*10)+temp;         n=n/10;     }     x=sum*sum;     while(x>0)     {         semp=x%10;         rev=(rev*10)+semp;         x=x/10;     }     if(rev==m)     {         printf("True");     }     else     {         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int k,n,temp=0,rev=0,sum=0,m;
+    int n,sum=0,m,x,temp,semp,rev=0;
     scanf("%d",&n);
     m=n*n;
     while(n>0)
     {
         temp=n%10;
-        rev=rev*10+temp;
+        sum=(sum*10)+temp;
         n=n/10;
     }
-    k=rev*rev;
-    while(k>0)
+    x=sum*sum;
+    while(x>0)
     {
-        temp=k%10;
-        sum=sum*10+temp;
-        k=k/10;
+        semp=x%10;
+        rev=(rev*10)+semp;
+        x=x/10;
     }
-    if(sum==m)
+    if(rev==m)
     {
         printf("True");
     }
