74d06a8de1fb8fd8f8aa9b934b6ebc3d0900e5d2
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-05-28 18:59:58 +0530

    #include<stdio.h> int main() {     int a,b,i,suma=0,sumb=0;     scanf("%d",&a);     scanf(" %d",&b);     for(i=1;i<a;i++)     {         if(a%i==0)         {             suma=suma+i;         }     }     for(i=1;i<b;i++)     {         if(b%i==0)         {             sumb=sumb+i;         }     }     if(a==sumb && b==suma)     {         printf("Amicable");     }     else     {         printf("Not Amicable");     } }

---
+++ Amicable_Numbers.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,suma=0,sumb=0;
+    scanf("%d",&a);
+    scanf("
+%d",&b);
+    for(i=1;i<a;i++)
+    {
+        if(a%i==0)
+        {
+            suma=suma+i;
+        }
+    }
+    for(i=1;i<b;i++)
+    {
+        if(b%i==0)
+        {
+            sumb=sumb+i;
+        }
+    }
+    if(a==sumb && b==suma)
+    {
+        printf("Amicable");
+    }
+    else
+    {
+        printf("Not Amicable");
+    }
+}
