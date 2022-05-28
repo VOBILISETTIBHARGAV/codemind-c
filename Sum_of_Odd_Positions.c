d17d060dbbc287e6249501d097e48c2287674c0a
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-05-28 19:37:41 +0530

    #include<stdio.h> int main() {     int size,sum=0;     scanf("%d",&size);     int arr[size],i;     for(i=0;i<size;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<size;i++)     {         if(i%2!=0)         {            sum=sum+arr[i];         }     }     printf("%d",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int size,sum=0;
+    scanf("%d",&size);
+    int arr[size],i;
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<size;i++)
+    {
+        if(i%2!=0)
+        {
+           sum=sum+arr[i];
+        }
+    }
+    printf("%d",sum);
+}
