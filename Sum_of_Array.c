372f1cde5a5ec56616bf1fc1e2a412ca3fc4ec3f
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-05-28 19:43:15 +0530

    #include<stdio.h> int main() {     int size,sum=0;     scanf("%d",&size);     int arr[size],i;     for(i=0;i<size;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<size;i++)     {            sum=sum+arr[i];     }     printf("%d",sum); }

---
+++ Sum_of_Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int size,sum=0;
+    scanf("%d",&size);
+    int arr[size],i;
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<size;i++)
+    {
+           sum=sum+arr[i];
+    }
+    printf("%d",sum);
+}
