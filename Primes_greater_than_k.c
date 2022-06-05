7f769a2b936969be447e9f627d79b1fc1ff829db
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-06-05 22:28:27 +0530

    #include<stdio.h> int prime(int n) {     int i,count=0;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             count++;         }     }     if(count==2)     {         return 1;     }     else     {         return 0;     } } int main() {     int count=0,k,n;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(arr[i]>=k)         {             if(prime(arr[i])==1)             {               count++;             }         }     }     printf("%d",count); }

---
+++ Primes_greater_than_k.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int prime(int n)
+{
+    int i,count=0;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+        }
+    }
+    if(count==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int count=0,k,n;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=k)
+        {
+            if(prime(arr[i])==1)
+            {
+              count++;
+            }
+        }
+    }
+    printf("%d",count);
+}
