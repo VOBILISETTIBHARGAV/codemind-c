ae63530604106d957684e89aaeadea85ac73789f
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-06-05 22:18:14 +0530

    #include<stdio.h> int main() {     int size,sum=0;     scanf("%d",&size);     int arr[size],i;     for(i=0;i<size;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<size;i++)     {         if(arr[i]%2!=0)         {             sum=sum+arr[i];         }         else         {             break;         }     }     printf("%d",sum); }

---
+++ sum_of_array__elements_upto__first_even_number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int size,sum=0;
+    scanf("%d",&size);
+    int arr[size],i;
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<size;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            sum=sum+arr[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("%d",sum);
+}
