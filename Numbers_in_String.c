d06f329fd5cc477cbc40a2dec30888f505a8c79b
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-06-13 22:40:33 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100],i,sum=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {          if(str[i]>=48 && str[i]<=57)          {              sum=sum+str[i]-48;          }     }     printf("%d",sum); }

---
+++ Numbers_in_String.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100],i,sum=0;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+         if(str[i]>=48 && str[i]<=57)
+         {
+             sum=sum+str[i]-48;
+         }
+    }
+    printf("%d",sum);
+}
