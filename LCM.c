c06a0b6dedb9f30092ef7f162565594d872e0e80
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-06-29 21:35:43 +0530

    #include<stdio.h> int main() {     int i,a,b,temp=1,lcm;     scanf("%d%d",&a,&b);     for(i=1;i<=a && i<=b;i++)     {         if(a%i==0 && b%i==0)         {            temp=i;         }     }     lcm=a*b/temp;     printf("%d",lcm); }

--- LCM.c
+++ LCM.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
-{ 
-    int n1,n2,i,gcd,lcm; 
-    scanf("%d%d",&n1,&n2); 
-    for(i=1;i<=n1&&i<=n2;++i) 
-    { 
-        if (n1%i==0&&n2%i==0) 
-        gcd=i; 
-    } 
-    lcm=(n1*n2)/gcd; 
-    printf("%d",lcm); 
-    return 0; 
+{
+    int i,a,b,temp=1,lcm;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=a && i<=b;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+           temp=i;
+        }
+    }
+    lcm=a*b/temp;
+    printf("%d",lcm);
 }
