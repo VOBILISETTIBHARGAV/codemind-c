149d3396db681ee37f90880c3059bf06e387b925
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-06-29 21:11:22 +0530

    #include<stdio.h> int primes_range(int num) {     int i,count=0;     for(i=1;i<=num;i++)     {       if(num%i==0)       {         count++;       }     }     if(count<=2)     {         return 1;     }     else     {         return 0;     } } int main() {     int n,sum=0,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {      if(n%i==0)       {        if(primes_range(i)==0)         {           sum++;         }       }     }     printf("%d",sum+1); }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-int prime(int n)
+int primes_range(int num)
 {
     int i,count=0;
-    for(i=1;i<=n;i++)
+    for(i=1;i<=num;i++)
     {
-        if(n%i==0)
-        {
-            count++;
-        }
+      if(num%i==0)
+      {
+        count++;
+      }
     }
-    if(count==2)
+    if(count<=2)
     {
         return 1;
     }
@@ -20,17 +20,17 @@ int prime(int n)
 }
 int main()
 {
-    int n,i,sum=0;
+    int n,sum=0,i;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        if(n%i==0)
+     if(n%i==0)
+      {
+       if(primes_range(i)==0)
         {
-          if(prime(i)==0)
-           {
-            sum=sum+1;
-           }
+          sum++;
         }
+      }
     }
-    printf("%d",sum);
+    printf("%d",sum+1);
 }
