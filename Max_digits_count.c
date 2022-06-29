ea0545841671ae02cfae42f712f5a7e88bcaa519
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-06-30 04:00:34 +0530

    #include<stdio.h> int digits(int n) {     int count=0;     while(n>0)     {         n=n/10;         count++;     }     return count; } int main() {     int n,count=0,min;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     min=digits(arr[0]);     for(i=0;i<n;i++)     {         if(digits(arr[i])>min)         {             min=digits(arr[i]);         }     }     for(i=0;i<n;i++)     {         if(digits(arr[i])==min)         {             count++;         }     }     printf("%d",count); }

---
+++ Max_digits_count.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int digits(int n)
+{
+    int count=0;
+    while(n>0)
+    {
+        n=n/10;
+        count++;
+    }
+    return count;
+}
+int main()
+{
+    int n,count=0,min;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    min=digits(arr[0]);
+    for(i=0;i<n;i++)
+    {
+        if(digits(arr[i])>min)
+        {
+            min=digits(arr[i]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(digits(arr[i])==min)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
