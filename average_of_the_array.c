9d4f48ac712f415209683fad02e72eec58f16280
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-06-30 21:47:03 +0530

    #include<stdio.h> int main() {     int flag=0,size;     float x,sum=0;     scanf("%d",&size);     int arr[size],i;     for(i=0;i<size;i++)     {         scanf("%d",&arr[i]);     }    for(i=0;i<size;i++)    {        sum=sum+arr[i];    }    x=sum/size;    printf("%.2f",x); }

--- average_of_the_array.c
+++ average_of_the_array.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int size,flag=0;
-    float sum=0,x;
+    int flag=0,size;
+    float x,sum=0;
     scanf("%d",&size);
     int arr[size],i;
     for(i=0;i<size;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=0;i<size;i++)
-    {
-        sum=sum+arr[i];
-    }
-    x=sum/size;
-    printf("%.2f",x);
+   for(i=0;i<size;i++)
+   {
+       sum=sum+arr[i];
+   }
+   x=sum/size;
+   printf("%.2f",x);
 }
