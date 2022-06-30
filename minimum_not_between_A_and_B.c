52c99c0b216e426f40696d5f9516c3a7baf2b64a
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-06-30 07:09:18 +0530

    #include<stdio.h> int main() {     int n,count=0,sum=0,a,b,min;     scanf("%d",&n);     int arr[n],i,arr1[100],arr2[100],j=0,k=0;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     min=100;     for(i=0;i<n;i++)     {         if(arr[i]<a || arr[i]>b)         {             if(arr[i]<min)             {                 min=arr[i];                 count++;             }         }     }     if(count>0)     {       printf("%d",min);     }     else     {       printf("-1");     } }

---
+++ minimum_not_between_A_and_B.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,count=0,sum=0,a,b,min;
+    scanf("%d",&n);
+    int arr[n],i,arr1[100],arr2[100],j=0,k=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    min=100;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<a || arr[i]>b)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+                count++;
+            }
+        }
+    }
+    if(count>0)
+    {
+      printf("%d",min);
+    }
+    else 
+    {
+      printf("-1");
+    }
+}
