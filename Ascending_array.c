7092b98b1a753409f7f5503dcd85280bceaee576
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-06-30 06:44:45 +0530

    #include<stdio.h> int main() { int size,sum=0; scanf("%d", &size); int arr[size],i; for(i=0;i<size;i++) { scanf("%d",&arr[i]); } for(i=0;i<size-1;i++) { if(arr[i]<arr[i+1]) { sum++; } else {     break; } } if(size-1==sum) {     printf("yes"); } else {     printf("no"); } }

---
+++ Ascending_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+int size,sum=0; 
+scanf("%d", &size);
+int arr[size],i; 
+for(i=0;i<size;i++)
+{
+scanf("%d",&arr[i]);
+}
+for(i=0;i<size-1;i++)
+{
+if(arr[i]<arr[i+1])
+{
+sum++;
+}
+else
+{
+    break;
+}
+}
+if(size-1==sum)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+}
