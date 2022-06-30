bc4d222acb074d92d283d5ff12ee2cefed38138b
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-06-30 21:50:13 +0530

    #include<stdio.h> int main() {     int size,min;     scanf("%d",&size);     int arr[size],i;     for(i=0;i<size;i++)     {         scanf("%d",&arr[i]);     }      min=arr[0];     for(i=0;i<size;i++)     {         if(min<arr[i])         {             min=arr[i];         }     }     printf("%d",min); }

--- maximum_of_an_array.c
+++ maximum_of_an_array.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int min,size;
+    int size,min;
     scanf("%d",&size);
     int arr[size],i;
     for(i=0;i<size;i++)
     {
         scanf("%d",&arr[i]);
     }
-    min=arr[0];
+     min=arr[0];
     for(i=0;i<size;i++)
     {
         if(min<arr[i])
