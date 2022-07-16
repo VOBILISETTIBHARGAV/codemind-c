9e114a84da4b531e9700a4e0f60e84f901afa9fd
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-07-16 13:25:36 +0530

    #include<stdio.h> int main() {     int n,m,sum=0;     scanf("%d%d",&n,&m);     int arr[m][n],i,j;     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             if(i!=0 && j!=0 && i!=n-1 && j!=m-1)             {                 sum=sum+arr[i][j];             }         }     }     printf("%d",sum); }

---
+++ Sum_of_non_boundary_values_in_a_matrix.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,sum=0;
+    scanf("%d%d",&n,&m);
+    int arr[m][n],i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(i!=0 && j!=0 && i!=n-1 && j!=m-1)
+            {
+                sum=sum+arr[i][j];
+            }
+        }
+    }
+    printf("%d",sum);
+}
