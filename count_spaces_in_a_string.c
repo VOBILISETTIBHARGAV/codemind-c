335862669b46cf58aadf37fab9a806f12192f56b
VOBILISETTIBHARGAV <97425638+VOBILISETTIBHARGAV@users.noreply.github.com>
2022-07-16 20:01:59 +0530

    #include<stdio.h> int main() {     char str[30],i,count=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]==32)         {             count++;         }     }     if(count>0)     {         printf("%d",count);     } }

---
+++ count_spaces_in_a_string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char str[30],i,count=0;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]==32)
+        {
+            count++;
+        }
+    }
+    if(count>0)
+    {
+        printf("%d",count);
+    }
+}
